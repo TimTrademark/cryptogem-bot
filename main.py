5d727cd89a2c7dc8f9ae3cff8f023301377bf8d7
Tim Truyens <truyens.tim@telenet.be>
2024-05-05 19:22:27 +0200

    fix: MEXC connect get latest pairs

--- main.py
+++ main.py
@@ -20,17 +20,23 @@ from src.utils.BrandingUtils import get_logo_ascii_art
 from src.utils.PairUtils import PairUtils
 from src.utils.VersionUtils import read_version
 
-app = Flask(__name__, template_folder='control_panel', static_url_path='',
-            static_folder='control_panel/static', )
-app.add_url_rule('/', view_func=ControlPanelController.index, methods=["GET"])
+app = Flask(
+    __name__,
+    template_folder="control_panel",
+    static_url_path="",
+    static_folder="control_panel/static",
+)
+app.add_url_rule("/", view_func=ControlPanelController.index, methods=["GET"])
 app.register_blueprint(ConfigController.bp)
 app.debug = False
 CORS(app)
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 port = os.environ.get("CONTROL_PANEL_PORT")
-logging.basicConfig(format='[%(asctime)s] - %(message)s', datefmt='%d-%m-%y %H:%M', level=logging.INFO)
-flaskLogger = logging.getLogger('werkzeug')
+logging.basicConfig(
+    format="[%(asctime)s] - %(message)s", datefmt="%d-%m-%y %H:%M", level=logging.INFO
+)
+flaskLogger = logging.getLogger("werkzeug")
 flaskLogger.setLevel(logging.ERROR)
 logger = logging.getLogger("mycryptogem")
 logger.setLevel(logging.INFO)
@@ -42,35 +48,51 @@ def main():
     config_loader = ConfigLoader()
     config = config_loader.config
     last_non_tradable: Dict[str, List[Pair]] = {}
-    if os.environ.get("ENABLE_CONTROL_PANEL") is not None and os.environ.get("ENABLE_CONTROL_PANEL").upper() == 'TRUE':
+    if (
+        os.environ.get("ENABLE_CONTROL_PANEL") is not None
+        and os.environ.get("ENABLE_CONTROL_PANEL").upper() == "TRUE"
+    ):
         threading.Thread(target=lambda: start_flask(config_loader), daemon=True).start()
     logger.info("Started control panel")
     logger.info("Initializing exchanges...")
     init_last_non_tradable(config_loader.connectors, last_non_tradable)
-    config_loader.append_add_callback(lambda: init_last_non_tradable(config_loader.connectors, last_non_tradable))
+    config_loader.append_add_callback(
+        lambda: init_last_non_tradable(config_loader.connectors, last_non_tradable)
+    )
     logger.info("Started scanning exchanges")
     while True:
         try:
             config = config_loader.config
             connectors: List[ExchangeConnector] = config_loader.connectors
             for connector in connectors:
-                ec: ExchangeConfig = config.get_exchange_config_by_name(connector.get_connector_name())
+                ec: ExchangeConfig = config.get_exchange_config_by_name(
+                    connector.get_connector_name()
+                )
                 if not ec.active:
                     continue
                 latest_pairs = connector.get_latest_pairs()
-                compare_latest_pairs_and_trade(connector, latest_pairs, ec.funds,
-                                               last_non_tradable[connector.get_connector_name().lower()])
-                last_non_tradable[connector.get_connector_name().lower()] = PairUtils.filter_non_tradable_pairs(
-                    latest_pairs)
+                compare_latest_pairs_and_trade(
+                    connector,
+                    latest_pairs,
+                    ec.funds,
+                    last_non_tradable[connector.get_connector_name().lower()],
+                )
+                last_non_tradable[connector.get_connector_name().lower()] = (
+                    PairUtils.filter_non_tradable_pairs(latest_pairs)
+                )
 
         except Exception as e:
             print(e)
         time.sleep(config.scheduling_timeout_seconds)
 
 
-def init_last_non_tradable(connectors: List[ExchangeConnector], last_non_tradable: Dict[str, List[Pair]]):
+def init_last_non_tradable(
+    connectors: List[ExchangeConnector], last_non_tradable: Dict[str, List[Pair]]
+):
     for c in connectors:
-        last_non_tradable[c.get_connector_name().lower()] = PairUtils.filter_non_tradable_pairs(c.get_latest_pairs())
+        last_non_tradable[c.get_connector_name().lower()] = (
+            PairUtils.filter_non_tradable_pairs(c.get_latest_pairs())
+        )
 
 
 def start_flask(config_loader: ConfigLoader):
@@ -78,13 +100,19 @@ def start_flask(config_loader: ConfigLoader):
     app.run(host="0.0.0.0", port=port or 5000)
 
 
-def compare_latest_pairs_and_trade(connector: ExchangeConnector, latest_pairs: List[Pair], funds: float,
-                                   last_non_tradable: List[Pair]):
+def compare_latest_pairs_and_trade(
+    connector: ExchangeConnector,
+    latest_pairs: List[Pair],
+    funds: float,
+    last_non_tradable: List[Pair],
+):
     logger.debug("Latest pairs:")
     logger.debug(list(map(lambda lp: str(lp), latest_pairs)))
     for p in latest_pairs:
         if p.tradable and PairUtils.pair_exists_in_list(last_non_tradable, p):
-            logger.info(f"{str(p)} has opened trading on {connector.get_connector_name()}, buying coin...")
+            logger.info(
+                f"{str(p)} has opened trading on {connector.get_connector_name()}, buying coin..."
+            )
             buy(p, funds, connector)
 
 
@@ -92,5 +120,5 @@ def buy(pair: Pair, funds: float, connector: ExchangeConnector):
     connector.execute_buy_order(pair, funds)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
